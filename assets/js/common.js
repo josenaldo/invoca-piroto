f4bb75ec8a5ede049e89d1460d8ba07663d47e3f
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2020-06-04 07:11:23 -0300

    Página de nomes com Datatables

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,10 +5,19 @@ requirejs.config({
     paths: {
         "jquery": "//code.jquery.com/jquery-3.5.1.min",
         "bootstrap": "//stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min",
+        "datatables": "//cdn.datatables.net/1.10.21/js/jquery.dataTables.min",
+        "datatables.bootstrap": "//cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min",
+        "datatables.responsive": "//cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min",
         "lazy": "../lazy",
         "main": "Main",
     },
     shim: {
-        "bootstrap": ['jquery']
+        "bootstrap": ["jquery"],
+    },
+    map: {
+        '*': {
+            'datatables.net': 'datatables',
+        }
     },
 });
+
---
+++ assets/js/modules/PageNomes.js
@@ -0,0 +1,29 @@
+define(
+    ["jquery", "datatables.bootstrap" ],
+    function ( $, ) {
+        $(document).ready(function() {
+            $('#nomes').dataTable({
+                "paging":   true,
+                "ordering": true,
+                'searching': true,
+                "pageLength": 10,
+                "pagingType": "full_numbers",
+                "language": {
+                    "search": "Busca",
+                    "lengthMenu": "Mostrando _MENU_ capirotos por página",
+                    "zeroRecords": "Nenhum capiroto sumonado",
+                    "info": "Mostrando página _PAGE_ de _PAGES_",
+                    "infoEmpty": "Nenhum capiroto no inferno",
+                    "infoFiltered": "(filtrado de _MAX_ total de capirotos)",
+                    "paginate":{
+                        "first": "Primeiro",
+                        "previous": "Anterior",
+                        "next": "Próximo",
+                        "last": "Último",
+                    }
+                }
+
+            });
+        } );
+    }
+);
